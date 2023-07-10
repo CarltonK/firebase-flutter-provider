c104936250dd608a8333926257bec384f527ccb6
Mark Carlton <mcarlton33@gmail.com>
2023-07-10 18:32:41 +0300

    backend setup

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   root: true,
   env: {
+    browser: true,
     es6: true,
     node: true,
   },
@@ -25,8 +26,57 @@ module.exports = {
     'import',
   ],
   rules: {
-    'quotes': ['error', 'single'],
-    'import/no-unresolved': 0,
-    'indent': ['error', 2],
+    '@typescript-eslint/adjacent-overload-signatures': 'error',
+    '@typescript-eslint/no-empty-function': 'error',
+    '@typescript-eslint/no-empty-interface': 'warn',
+    '@typescript-eslint/no-floating-promises': 'error',
+    '@typescript-eslint/no-namespace': 'error',
+    '@typescript-eslint/no-unnecessary-type-assertion': 'error',
+    '@typescript-eslint/prefer-for-of': 'warn',
+    '@typescript-eslint/triple-slash-reference': 'error',
+    '@typescript-eslint/unified-signatures': 'warn',
+    'comma-dangle': ['error', 'always-multiline'],
+    'constructor-super': 'error',
+    eqeqeq: ['warn', 'always'],
+    'import/no-deprecated': 'warn',
+    'import/no-extraneous-dependencies': 'error',
+    'import/no-unassigned-import': 'warn',
+    'no-cond-assign': 'error',
+    'no-duplicate-case': 'error',
+    'no-duplicate-imports': 'error',
+    'no-empty': [
+      'error',
+      {
+        allowEmptyCatch: true,
+      },
+    ],
+    'no-invalid-this': 'error',
+    'no-new-wrappers': 'error',
+    'no-param-reassign': 'error',
+    'no-redeclare': 'error',
+    'no-sequences': 'error',
+    'no-shadow': [
+      'error',
+      {
+        hoist: 'all',
+      },
+    ],
+    'no-throw-literal': 'error',
+    'no-unsafe-finally': 'error',
+    'no-unused-labels': 'error',
+    'no-var': 'warn',
+    'no-void': 'error',
+    'prefer-const': 'warn',
+    'object-curly-spacing': 'off',
+    'indent': 'off',
+    'max-len': 'off',
+    'import/no-unresolved': 'off',
+  },
+  settings: {
+    jsdoc: {
+      tagNamePreference: {
+        returns: 'return',
+      },
+    },
   },
 };
